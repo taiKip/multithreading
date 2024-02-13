4bde006566c1069de1c2671c85cc59c3b0b50c6c
tarus <victortarus6@gmail.com>
2024-02-13 19:03:17 +0200

    added thread using lambda

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,6 +8,19 @@ var currentThread = Thread.currentThread();
 printThreadState(currentThread);
 CustomThread customThread = new CustomThread();
 customThread.start();
+
+Runnable myRunnable = ()->{
+    for(int i=1;i<=8;i++){
+        System.out.print(" 2 ");
+        try{
+            TimeUnit.MILLISECONDS.sleep(250);
+        } catch (InterruptedException e) {
+         e.printStackTrace();
+        }
+    }
+};
+Thread  myThread = new Thread(myRunnable);
+myThread.start();
 for(int i = 1;i<= 3;i++){
     System.out.print(" 0 ");
     try{
