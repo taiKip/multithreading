2fb75684af67fd72c684d270b897586e34aed371
tarus <victortarus6@gmail.com>
2024-02-13 13:45:26 +0200

    Threads info

--- src/main/java/org/example/CustomThread.java
+++ src/main/java/org/example/CustomThread.java
@@ -1,13 +1,15 @@
 package org.example;
 
+import java.util.concurrent.TimeUnit;
+
 public class CustomThread extends Thread{
     @Override
     public void run() {
-       for (int i = 1;i<=5;i++){
+       for(int i =1;i<=5;i++){
            System.out.print(" 1 ");
-           try{
-Thread.sleep(500); // Adding half a second delay between each count
-           }catch(InterruptedException e) {
+           try {
+               Thread.sleep(500);
+           }catch (InterruptedException e){
                e.printStackTrace();
            }
        }
